9c5417884d1147d4422cf8fd29e5d61f845ccd66
yann2084 <y.vernette@gmail.com>
2016-08-22 18:44:15 +0200

    Add files via upload

--- modules/tests/source_villesAvion.php
+++ modules/tests/source_villesAvion.php
@@ -1,18 +1,10 @@
 <?php
 include_once("../../model/DAO.class.php");
 
-
-
-/* veillez bien à vous connecter à votre base de données */
 $term = "";
 if(isset($_GET['term'])) $term = $_GET['term'];
 
-//$requete = $bdd->prepare('SELECT * FROM membres WHERE pseudo LIKE :term'); // j'effectue ma requête SQL grâce au mot-clé LIKE
-//$requete->execute(array('term' => '%'.$term.'%'));
-
-
-
-$dao->getVilleAvion($term);
+$dao->getVillesAvion($term);
 
 
 ?>
--- modules/tests/test_autocompleteMultiSource.view.php
+++ modules/tests/test_autocompleteMultiSource.view.php
@@ -1,52 +1,59 @@
-
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <title>Remboursement mission</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="../../img/logoForm.png" type="image/x-icon" />
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../view/stylesheet.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" type="text/css" href="../../modules/nouvelle_demande/js/calculator/jquery.calculator.css">
+    
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
     
+    <script src='../../modules/nouvelle_demande/jquery.validate.min.js'></script>
+    <script src='../../modules/nouvelle_demande/additional-methods.min.js'></script>
+    
+        <script type="text/javascript" src="../../modules/nouvelle_demande/js/calculator/jquery.plugin.js"></script>
+    <script type="text/javascript" src="../../modules/nouvelle_demande/js/calculator/jquery.calculator.js"></script>
+    <script type="text/javascript" src="../../modules/nouvelle_demande/js/calculator/jquery.calculator-fr.js"></script>
+
+    
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-
-
-
-    <br><br><br><br>
     <div id="toto"></div>
     <form method="get" action="insererville.php">
-        <input type="text" id="tata" name="ville"/>
+        <input type="text" id="champ" name="ville"/>
         <input type="submit" value="Submit">
     </form>
     <script>
 
-
-    function autocompVille(){
-        var depart1 = (document.getElementById("tata"));
+    function paspossible(){
+        var depart1 = document.getElementById("champ");
         var autocomplete = new google.maps.places.Autocomplete(depart1);
     }
 
 
-    $("#tata").autocomplete({
-        source: [ "Grenoble, France", "Lyon, France", "Chambéry, France", "Chambéry, France", "Paris, France", "Valence, France", "Gières, France" ],
+    $("#champ").autocomplete({
+        source: [ "Grenoble, France", "Lyon, France", "Chambéry, France", "Paris, France", "Valence, France", "Gières, France" ],
         response: function(event, ui)
 		{
             // ui.content is the array that's about to be sent to the response callback.
             document.getElementById('toto').innerHTML = ui.content.length;
             if (ui.content.length === 0)
 			{
-				$("#tata").autocomplete({
+				$("#champ").autocomplete({
 					source : 'source_villesAvion.php',
 					response: function(event, ui)
 					{
 						if (ui.content.length === 0)
 						{
-							autocompVille();
+							paspossible();
 						}
 					}
 				});
@@ -55,6 +62,10 @@
 	});
 
         </script>
+        
+<script src="../nouvelle_demande/js/myScript.js"></script>
+<script src="../nouvelle_demande/js/scriptEtapes.js"></script>
+
         
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDprc5sfu1qrH8ChZ18E3q8_xijqIOTJGQ&signed_in=true&libraries=places&callback=initMap" async defer></script>
 </body>
